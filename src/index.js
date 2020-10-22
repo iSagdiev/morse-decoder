63268a3461cfb50c2305ea002fcd27fdd512e843
iSagdiev <jay.iskander@gmail.com>
2020-10-22 21:41:20 +0500

    init: uploading task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = [];
+    let newStr = '';
+    let tempArr = [];
+    let newArr = [];
+    for (let i = 0; i < expr.length; i = i + 2) {
+        let temp = expr.substring(i, i+2);
+        if (temp === '10') {
+            newStr += '.';
+        } else if (temp === '11') {
+            newStr += '-';
+        } else if (temp === '**') {
+            newStr += '*'; 
+        } else if (temp === '00') {
+          newStr += '0';
+        }
+    }
+    
+    for (let i = 0; i < newStr.length; i = i + 5) {
+        arr.push(newStr.substring(i, i+5));
+        
+    }
+    for (let i = 0; i < arr.length; i++) {
+      let temp = '';
+      for (let j = 0; j < arr[i].length; j++) {
+        if (arr[i][j] !== '0') {
+          temp += arr[i][j];
+        }
+      }
+      tempArr.push(temp);
+      
+    }
+    for (let i = 0; i < tempArr.length; i++) {
+        if (tempArr[i] === '*****') {
+            newArr.push(' ');
+        } else {
+          newArr.push(MORSE_TABLE[tempArr[i]]);
+        }     
+    }
+    
+    return(newArr.join(''));
+  }
 
 module.exports = {
     decode
